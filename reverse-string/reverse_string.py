9cbbd66cef83a6caf12b8e40d6cb01083b8e1788
Hemabh Kamboj <hkamboj2603199@gmail.com>
2018-08-22 20:08:23 +0530

    added reverse string

--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,18 +1,11 @@
 
 def reverse(text):
-    str=""
-    for i in text:
-        str = i + str
-        return(str)
-   # print(i)
-    #while i >= 1:
-     #   print(text)
-      #  i = i-1
+    return  text[::-1]
+
 
         
 
 text = str(input("Enter string to be reversed:  "))
-print (type(str))
 print (reverse(text))       
     
 
